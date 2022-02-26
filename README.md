d8d6d44842c762b036a5a5bf9b5d77c7ad5574d1
Haotian Wang <haotian@duolingo.com>
2022-02-26 16:04:53 -0500

    Add config file support

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 ## Development
 Some set up instructions for local development.
 
-### Python Setup (for web)
+### Web Backend Setup
 This project uses Python 3.9. Please create a virtual environment and upgrade your pip.
 ```bash
 cd web
@@ -13,6 +13,10 @@ pip install -U pip wheel pip-tools
 pip install -r dev-requirements.txt
 pre-commit install
 ```
+
+Note that the web backend uses the environment variable `CONFIG_ENV` to choose the config
+yaml file. The default is `dev.yml` if the environment variable is unspecified, and the web
+backend will use the [corresponding config file](config/dev.yml).
 
 ### Docker Setup
 You can run `docker compose build && docker compose up` to spin up a development server.
---
+++ web/utils/config.py
@@ -0,0 +1,43 @@
+"""Module for loading yaml configs"""
+import os
+from types import SimpleNamespace
+from typing import Dict
+
+import yaml
+from utils.constants import ENCODING
+
+try:
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Loader
+
+
+class ConfigInitializeException(Exception):
+    """Exceptions that occur when a config is initialized."""
+
+
+class Config(SimpleNamespace):  # pylint: disable=too-few-public-methods
+    """Actual class that reflects dictionaries into object attributes."""
+
+    def __init__(self, data: Dict) -> None:
+        for key, value in data.items():
+            if isinstance(value, (list, tuple, frozenset)):
+                setattr(self, key, tuple(Config(value_entry) for value_entry in value))
+            elif isinstance(value, dict):
+                setattr(self, key, Config(value))
+            else:
+                setattr(self, key, value)
+
+
+def get_config() -> Config:
+    """Get the config object from environment variable."""
+    config_env = os.environ.get("CONFIG_ENV")
+    if config_env is None:
+        print("`CONFIG_ENV` is not defined, using `dev.yml` instead")
+        config_env = "dev.yml"
+    yaml_data = None
+    with open(f"config/{config_env}", "r", encoding=ENCODING) as config_file:
+        yaml_data = yaml.load(config_file, Loader=Loader)
+    if yaml_data is None:
+        raise ConfigInitializeException(f"config/{config_env} does not contain valid data")
+    return Config(yaml_data)
