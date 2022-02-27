a7bccf73e124482f0a4b23c4e7561e0d095099dc
Haotian Wang <haotian@duolingo.com>
2022-02-27 13:29:42 -0500

    Fix internal route that gets all yields

--- web/views/internal.py
+++ web/views/internal.py
@@ -1,10 +1,19 @@
 """API routes that are meant for developers."""
 from application import app
-from flask import jsonify
+from flask import jsonify, request
 from models.series_data import ApySeriesData
 
 
 @app.route("/raw-yields")
 def get_raw_yields():
-    """Get all the raw yields in the database."""
-    return jsonify(ApySeriesData.query.all())
+    """
+    Get some random raw yields in the database.
+    The number of returned records will be capped by `get_limit`, which defaults to 10.
+    """
+    get_limit = int(request.args.get("get_limit", "10"))
+    return jsonify(
+        tuple(
+            row._asdict()
+            for row in ApySeriesData.query.with_entities(ApySeriesData.pool_info_name, ApySeriesData.pool_yield).limit(get_limit).all()
+        )
+    )
--- web/workers/store_beefy.py
+++ web/workers/store_beefy.py
@@ -10,16 +10,14 @@ from init import db
 from models.series_data import ApySeriesData, PoolInfo
 from utils.config import APPLICATION_CONFIG
 
-_BEEFY_APY_ENDPOINT = "https://api.beefy.finance/apy"
-
 _fifo_queue: queue.SimpleQueue = queue.SimpleQueue()
 _wait_event_obj: threading.Event = threading.Event()
 
 
-def fetch_beefy_yield(app: Flask) -> None:
+def fetch_beefy_yield() -> None:
     """Fetch beefy api every second and put into queue."""
     while True:
-        response = requests.get(_BEEFY_APY_ENDPOINT)
+        response = requests.get(APPLICATION_CONFIG.yield_endpoint.beefy)
         if response.status_code == requests.codes.OK:  # pylint: disable=no-member
             _fifo_queue.put(response.content)
         _wait_event_obj.wait(APPLICATION_CONFIG.apy_api_worker.fetch_interval_seconds)
@@ -41,12 +39,12 @@ def insert_yields(app: Flask) -> None:
                 for pool_name, apy in content_dict.items():
                     if apy is None or pool_name is None:
                         continue
-                    missing_info = PoolInfo.query.filter_by(pool_name=pool_name).first()
-                    if missing_info is None:
-                        missing_info = PoolInfo(pool_name=pool_name)
+                    corresponding_pool_info = PoolInfo.query.get(pool_name)
+                    if corresponding_pool_info is None:
+                        corresponding_pool_info = PoolInfo(pool_name=pool_name)
                     db.session.add(
                         ApySeriesData(
-                            pool_info=missing_info,
+                            pool_info=corresponding_pool_info,
                             pool_yield=apy,
                         )
                     )
@@ -56,7 +54,7 @@ def insert_yields(app: Flask) -> None:
 
 def start_beefy_apy_workers(app: Flask) -> None:
     """Start fetch threads and insert threads."""
-    fetch_thread = threading.Thread(target=fetch_beefy_yield, args=(app,), daemon=True)
+    fetch_thread = threading.Thread(target=fetch_beefy_yield, daemon=True)
     insert_thread = threading.Thread(target=insert_yields, args=(app,), daemon=True)
     fetch_thread.start()
     insert_thread.start()
