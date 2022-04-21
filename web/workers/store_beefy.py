7f8976d755ffbf1350817b0afafbd1f82d8d9736
Jingchao Zhou <zhoujc999@gmail.com>
2022-04-20 22:10:10 -0700

    Update store_beefy.py

--- web/workers/store_beefy.py
+++ web/workers/store_beefy.py
@@ -15,41 +15,47 @@ from utils.config import APPLICATION_CONFIG
 def fetch_beefy_yield(fifo_queue: queue.SimpleQueue, wait_event_obj: threading.Event) -> None:
     """Fetch beefy api every second and put into queue."""
     while True:
-        response = requests.get(APPLICATION_CONFIG.yield_endpoint.beefy)
-        if response.status_code == requests.codes.OK:  # pylint: disable=no-member
-            fifo_queue.put((response.content, datetime.datetime.now(datetime.timezone.utc)))
+        try:
+            response = requests.get(APPLICATION_CONFIG.yield_endpoint.beefy)
+            if response.status_code == requests.codes.OK:  # pylint: disable=no-member
+                fifo_queue.put((response.content, datetime.datetime.now(datetime.timezone.utc)))
+        except Exception as e:
+            print(e)
         wait_event_obj.wait(APPLICATION_CONFIG.apy_api_worker.fetch_interval_seconds)
 
 
 def insert_yields(app: Flask, fifo_queue: queue.SimpleQueue, wait_event_obj: threading.Event) -> None:
     """Insert beefy api into database."""
     while True:
-        rough_size = fifo_queue.qsize()
-        raw_contents: List[Tuple[bytes, datetime.datetime]] = []
-        for _ in range(rough_size):
-            try:
-                raw_contents.append(fifo_queue.get_nowait())
-            except queue.Empty:
-                break
-        with app.app_context():
-            for raw_content, fetch_timestamp in raw_contents:
-                content_dict = json.loads(raw_content)
-                with db.session.no_autoflush:
-                    for pool_name, apy in content_dict.items():
-                        if apy is None or pool_name is None:
-                            continue
-                        corresponding_pool_info = db.session.get(PoolInfo, pool_name)
-                        if corresponding_pool_info is None:
-                            corresponding_pool_info = PoolInfo(pool_name=pool_name)
-                        db.session.add(
-                            ApySeriesData(
-                                created_at=fetch_timestamp,
-                                pool_info=corresponding_pool_info,
-                                pool_yield=apy,
+        try:
+            rough_size = fifo_queue.qsize()
+            raw_contents: List[Tuple[bytes, datetime.datetime]] = []
+            for _ in range(rough_size):
+                try:
+                    raw_contents.append(fifo_queue.get_nowait())
+                except queue.Empty:
+                    break
+            with app.app_context():
+                for raw_content, fetch_timestamp in raw_contents:
+                    content_dict = json.loads(raw_content)
+                    with db.session.no_autoflush:
+                        for pool_name, apy in content_dict.items():
+                            if apy is None or pool_name is None:
+                                continue
+                            corresponding_pool_info = db.session.get(PoolInfo, pool_name)
+                            if corresponding_pool_info is None:
+                                corresponding_pool_info = PoolInfo(pool_name=pool_name)
+                            db.session.add(
+                                ApySeriesData(
+                                    created_at=fetch_timestamp,
+                                    pool_info=corresponding_pool_info,
+                                    pool_yield=apy,
+                                )
                             )
-                        )
-                        db.session.flush()
-            db.session.commit()
+                            db.session.flush()
+                db.session.commit()
+        except Exception as e:
+            print(e)
         wait_event_obj.wait(APPLICATION_CONFIG.apy_api_worker.insert_interval_seconds)
 
 
