b58e63410a220d6b5eb30b3ac6109f229701479d
Haotian Wang <haotian@duolingo.com>
2022-04-21 23:42:57 -0400

    Make the client supports deletion of pool list

--- client/src/components/pool_list/PoolList.tsx
+++ client/src/components/pool_list/PoolList.tsx
@@ -1,35 +1,12 @@
 import * as React from "react";
 import axios from "axios";
-import { List, Button, ListItemText, ListItemButton } from "@mui/material";
+import { List, Button } from "@mui/material";
+import PoolListEntry from "./PoolListEntry";
 import CancellableAdd from "./CancellableAdd";
-import { PoolInList } from "../api_response/types";
 
 const PoolList = () => {
   const [poolListNames, setPoolListNames] = React.useState<string[]>([]);
   const [isAddingPoolList, setIsAddingPoolList] = React.useState(false);
-  const [isShowingPoolList, setIsShowingPoolList] = React.useState(false);
-  const [poolsInList, setPoolsInList] = React.useState<string[]>([]);
-
-  const handleClickOnPoolList = React.useCallback(
-    (poolListName: string) => async () => {
-      setIsShowingPoolList(prevState => !prevState);
-      try {
-        const response = await axios.get(
-          `/get-pools-by-list-name/${poolListName}`
-        );
-        setPoolsInList(
-          (response.data as PoolInList[])?.map(info => info.pool_name)
-        );
-      } catch (error) {
-        console.error(error);
-      }
-    },
-    []
-  );
-
-  const onClickAdd = () => setIsAddingPoolList(true);
-
-  const onCloseAdd = () => setIsAddingPoolList(false);
 
   const onClickGet = async () => {
     try {
@@ -40,6 +17,9 @@ const PoolList = () => {
     }
   };
 
+  const removePoolList = (poolListName: string) =>
+    setPoolListNames(poolListNames.filter(name => name != poolListName));
+
   return (
     <>
       <Button variant="contained" onClick={onClickGet}>
@@ -47,24 +27,17 @@ const PoolList = () => {
       </Button>
       <List>
         {poolListNames.map(poolListName => (
-          <React.Fragment key={poolListName}>
-            <ListItemButton onClick={handleClickOnPoolList(poolListName)}>
-              <ListItemText primary={poolListName} />
-            </ListItemButton>
-            {isShowingPoolList && (
-              <List>
-                {poolsInList.map(poolName => {
-                  return <ListItemText key={poolName} primary={poolName} />;
-                })}
-              </List>
-            )}
-          </React.Fragment>
+          <PoolListEntry
+            key={poolListName}
+            poolListName={poolListName}
+            onDeleteClicked={() => removePoolList(poolListName)}
+          />
         ))}
       </List>
       {isAddingPoolList ? (
-        <CancellableAdd onSubmitOrCancel={onCloseAdd} />
+        <CancellableAdd onSubmitOrCancel={() => setIsAddingPoolList(false)} />
       ) : (
-        <Button variant="contained" onClick={onClickAdd}>
+        <Button variant="contained" onClick={() => setIsAddingPoolList(true)}>
           Add new pool list!
         </Button>
       )}
---
+++ client/src/components/pool_list/PoolListEntry.tsx
@@ -0,0 +1,60 @@
+import { ListItemButton, ListItemText, List } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import React from "react";
+import axios from "axios";
+import { PoolInList } from "../api_response/types";
+
+interface PoolListEntryProps {
+  poolListName: string;
+  onDeleteClicked?: () => void;
+}
+
+const PoolListEntry = (props: PoolListEntryProps) => {
+  const [isShowingPoolList, setIsShowingPoolList] = React.useState(false);
+  const [poolsInList, setPoolsInList] = React.useState<string[]>([]);
+
+  const handleClickOnPoolList = React.useCallback(
+    (poolListName: string) => async () => {
+      setIsShowingPoolList(prevState => !prevState);
+      try {
+        const response = await axios.get(
+          `/get-pools-by-list-name/${poolListName}`
+        );
+        setPoolsInList(
+          (response.data as PoolInList[])?.map(info => info.pool_name)
+        );
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    []
+  );
+
+  const handleTrashCanClicked = async (event: React.MouseEvent) => {
+    event.stopPropagation();
+    try {
+      await axios.delete(`/delete-pool-list-by-name/${props.poolListName}`);
+      props.onDeleteClicked?.();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <React.Fragment key={props.poolListName}>
+      <ListItemButton onClick={handleClickOnPoolList(props.poolListName)}>
+        <ListItemText primary={props.poolListName} />
+        <DeleteIcon onClick={event => handleTrashCanClicked(event)} />
+      </ListItemButton>
+      {isShowingPoolList && (
+        <List>
+          {poolsInList.map(poolName => {
+            return <ListItemText key={poolName} primary={poolName} />;
+          })}
+        </List>
+      )}
+    </React.Fragment>
+  );
+};
+
+export default PoolListEntry;
